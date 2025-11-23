b04fbed2ebdb9eaae8042f2721bce389ec416487
Tanay Sah <sah_cse@hotmail.com>
2025-11-24 02:04:52 +0530

    Add files via upload

---
+++ 100daysofcodeday63.c
@@ -0,0 +1,42 @@
+// Day 63 - 100 Days of Code
+/* Write a program to take an integer array arr[] and an integer k as input.
+// Find the kth smallest element in the array and print it.
+*/
+// tanaysah 
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    if (k <= 0 || k > n) {
+        printf("Invalid value of k\n");
+        return 0;
+    }
+
+    
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+   
+    printf("%d\n", arr[k - 1]);
+
+    return 0;
+}
